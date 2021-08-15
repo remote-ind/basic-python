051ff057a4bb92a1799eaf971bb169a28d3b0dac
Ichsyan Fachrian <ichsyan.fachrian@gmail.com>
2021-08-15 15:10:13 +0700

    Initial Commit

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ print('Monday, 15 August 2021')
 print('Part One : Basic Python')
 print('-' * 27)
 
+# PERCABANGAN : EKSEKUSI TERPILIH
+fast_learn = True
+
+if fast_learn:
+    print('Learn everyday in the morning')
+else:
+    print('Just go to your bed and sleep or playing your phone!!!')
+
