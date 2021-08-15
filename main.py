bb0917f5bd11d291aea841314a373a5d9357d8a4
Ichsyan Fachrian <ichsyan.fachrian@gmail.com>
2021-08-15 15:01:37 +0700

    Initial Commit

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print ('Hello Ichsyan')
-print ('Welcome to RemoteWorker.id')
+# KONSTRUKSI DASAR PYTHON
+# SEQUENTIAL : EKSEKUSI BERURUTAN
+print('Hello Ichsyan')
+print('Welcome to RemoteWorker.id')
+print('Monday, 15 August 2021')
+print('Part One : Basic Python')
+print('-' * 27)
+
