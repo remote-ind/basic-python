1068e628dd72e8b9aab155ceeb6b04cc5c66591b
Ichsyan Fachrian <ichsyan.fachrian@gmail.com>
2021-11-11 06:45:45 +0700

    New Basic Syntax with Flowchart Guide

---
+++ basic-python-syntax-udemy.py
@@ -0,0 +1,20 @@
+"""
+New Basic Syntax with Flowchart
+"""
+
+print('My Mother said "Jack, can you buy me a bottle of milk?"')
+print('I said "Yes mom I will buy it"')
+print('Mother said "If the store sell eggs, buy 6 of egg"')
+print('I said "Aye mom!" and then I go to the store near home')
+
+milk = 40
+egg = 120
+
+if milk > 0 :
+    print("I check stock of Milk in the store, and")
+    if egg == 0:
+        print("I buy a bottle of Milk")
+    else:
+        print("I buy a bottle of Milk and 6 eggs")
+else :
+    print("I can not buy a bottle milk and egg")
