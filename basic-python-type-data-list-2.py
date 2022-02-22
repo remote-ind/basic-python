60a3adf6187f691d0c36381622878b78e57b3993
Ichsyan Fahcrian <ichsyan.fachrian@gmail.com>
2022-02-22 20:53:06 +0700

    New update from my journey

---
+++ basic-python-type-data-list-2.py
@@ -0,0 +1,13 @@
+# THIS IS MY NEW CHAPTER ABOUT TYPE DATA LIST IN PYTHON #
+print('Show Country Data List from Euro :')
+euro = ['Germany', 'Spain', 'England', 'France']
+print(euro)
+
+# SHOW THE DATA FROM SPECIFIC INDEX #
+print('\nShow Country Data List in Third Index :')
+print(euro[2]) # In Python, The Data List Start from 0 (Zero) #
+
+# SHOW THE DATA WITH REPETITION (FOR) #
+print('\nShow Country Data List with For :')
+for i in euro:
+    print(i)
