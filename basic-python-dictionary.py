c1ac10908c7853ccc823c5165c69fc2f9b6a54f0
Ichsyan Fahcrian <ichsyan.fachrian@gmail.com>
2022-05-15 21:42:57 +0700

    My Python Dictionary Learning File

---
+++ basic-python-dictionary.py
@@ -0,0 +1,24 @@
+# IN THIS MODUL WE WILL LEARN ABOUT PYTON DICTIONARY OR JSON OR REST API
+
+users = {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+}
+
+print(users)
+print(users['id'])
+print(users['name'])
+print(users['username'])
+print(users['email'])
